93fb584778aea8387108a0e9be2f6438233c574f
namnd01 <namnd1@itsol.vn>
2022-10-12 23:52:34 +0700

    up

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,11 +1,11 @@
 const siteMetadata = {
-  title: 'Nam </> Dev ',
+  title: 'Nam.Dev ',
   author: 'Nam Nguyen',
   headerTitle: 'TailwindBlog',
   description: `I'm a Front-end Developer in HaNoi!`,
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'http://namnd-dev.cf/_next/image?url=%2Fstatic%2Fimages%2FHTTP-Caching.gif&w=640&q=75',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
